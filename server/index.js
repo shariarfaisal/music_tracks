8b81ac41c5fde5e3712b4331a75f8c815c687ac8
shariarfaisal <faisaljr623@gmail.com>
2023-12-25 23:23:19 +0600

    fix: cors allowed to all

--- server/index.js
+++ server/index.js
@@ -9,7 +9,16 @@ const { addNewTrack } = require("./controllers/add_new_track");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+// allow all origins
+app.use(
+  cors({
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  })
+);
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
